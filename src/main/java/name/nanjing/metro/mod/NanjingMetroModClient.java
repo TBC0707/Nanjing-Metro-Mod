ef419ba52d309382e420cd9a0ad93e3d242d2682
滕柏晨 <Hello202229@outlook.com>
2025-01-10 17:16:05 +0800

    Nanjing-Metro-Mod

---
+++ src/main/java/name/nanjing/metro/mod/NanjingMetroModClient.java
@@ -0,0 +1,10 @@
+package name.nanjing.metro.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NanjingMetroModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
